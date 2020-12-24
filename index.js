2eb6424eed5e57155f19331eb95c7dd4bd8f8dfc
tanay-pingalkar <tpingalkar@gmail.com>
2020-12-24 17:04:23 +0500

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const http=require('http');
+const PORT= process.env.PORT || 500;
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(PORT);
 
 const TOKEN = process.env.yes;
 console.log(TOKEN)
