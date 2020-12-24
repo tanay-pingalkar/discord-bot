0d45d7a8b16956db6fb9d531dcb994a6b8d98f24
tanay-pingalkar <tpingalkar@gmail.com>
2020-12-24 16:57:27 +0500

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+
+const TOKEN = process.env.yes;
+console.log(TOKEN)
 
 bot.login(TOKEN);
 
@@ -10,16 +12,10 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+  if (msg.content.startsWith('hi')||msg.content.startsWith('Hi')) {
+    msg.reply('hello gorgeous');
+  }
+  else if(msg.content.startsWith('bye')){
+    msg.reply('bye see you later');
   }
 });
